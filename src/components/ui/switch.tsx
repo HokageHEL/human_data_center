13736c28adbae6f3c323f4e55cd1e77dec6bf100
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2025-07-06 03:00:33 +0300

    switcher on dark theme color issue fix

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
-import * as SwitchPrimitives from "@radix-ui/react-switch"
+import * as React from "react";
+import * as SwitchPrimitives from "@radix-ui/react-switch";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
@@ -17,11 +17,12 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-5 w-5 rounded-full shadow-lg ring-0 transition-transform data-[state=checked]:bg-foreground data-[state=unchecked]:bg-muted-foreground/50 data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
+        "pointer-events-none block h-5 w-5 rounded-full shadow-lg ring-0 transition-transform data-[state=checked]:bg-foreground data-[state=unchecked]:bg-muted-foreground/50 data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0",
+        "dark:data-[state=checked]:bg-gray-800 dark:data-[state=unchecked]:bg-gray-600"
       )}
     />
   </SwitchPrimitives.Root>
-))
-Switch.displayName = SwitchPrimitives.Root.displayName
+));
+Switch.displayName = SwitchPrimitives.Root.displayName;
 
-export { Switch }
+export { Switch };
